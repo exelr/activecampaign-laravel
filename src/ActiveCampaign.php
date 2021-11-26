2b97f5135890a40f620ce36e9350934440e301b0
Jure Bernava Prah <jure.bernavaprah@exelero.com>
2021-11-26 10:31:38 +0100

    fix: added resources

--- src/ActiveCampaign.php
+++ src/ActiveCampaign.php
@@ -3,21 +3,28 @@
 namespace Exelero\ActiveCampaignLaravel;
 
 use Exelero\ActiveCampaignLaravel\Resources\Accounts;
+use Exelero\ActiveCampaignLaravel\Resources\Contacts;
 
 class ActiveCampaign
 {
-    /**
-     * @var Accounts
-     */
-    public $accounts;
+    private Accounts $accounts;
+    private Contacts $contacts;
 
-    public function __construct(Accounts $accounts)
+    public function __construct(Accounts $accounts, Contacts $contacts)
     {
         $this->accounts = $accounts;
+        $this->contacts = $contacts;
     }
 
     public function accounts(): Accounts
     {
         return $this->accounts;
     }
+
+    public function contacts(): Contacts
+    {
+        return $this->contacts;
+    }
+
+
 }
